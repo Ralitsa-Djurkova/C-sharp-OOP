b26db156c64dae139e302d96bd8c3f95652b770d
Ralisa-Djurkova <ralitsadj@gmail.com>
2021-03-21 17:46:50 +0200

    Added delete bin and obj

---
+++ ReflectionsResurs/ValidationAttributes/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            if(obj != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
